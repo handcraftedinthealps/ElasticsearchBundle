23b14dee45f622d59668e91dc8bfb6577d19a8ab
Alexander Schranz <alexander@sulu.io>
2022-08-08 16:59:32 +0200

    Add support for monolog 3 (#30)
    * Add support for monolog 3
    
    * Fix compatibility to Monolog 3
    
    * Update Profiler/Handler/CollectionHandler.php
    
    Co-authored-by: Luca Rath-Heel <luca-rath@hotmail.com>
    
    Co-authored-by: Luca Rath-Heel <luca-rath@hotmail.com>

--- Profiler/Handler/CollectionHandler.php
+++ Profiler/Handler/CollectionHandler.php
@@ -12,23 +12,22 @@
 namespace ONGR\ElasticsearchBundle\Profiler\Handler;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
 
 /**
  * Handler that saves all records to him self.
  */
 class CollectionHandler extends AbstractProcessingHandler
 {
-    use BackwardCompatibilityWriteTrait;
-
     /**
-     * @var array
+     * @var array[]|LogRecord[]
      */
     private $records = [];
 
     /**
-     * {@inheritdoc}
+     * @param array|LogRecord $record
      */
-    protected function doWrite(array $record)
+    protected function write($record): void
     {
         $this->records[] = $record;
     }
@@ -36,7 +35,7 @@ class CollectionHandler extends AbstractProcessingHandler
     /**
      * Returns recorded data.
      *
-     * @return array
+     * @return array[]|LogRecord[]
      */
     public function getRecords()
     {
